e38d3d3fcdf5865323a0742db1ca794ef9e06ad9
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-05 11:14:12 +0530

    Time: 36 ms (79.45%) | Memory: 30.1 MB (37.79%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -7,9 +7,18 @@ public:
         if(s[i]==t[j]) return dp[i][j]= solve(i-1, j-1, s, t, dp)+ solve(i-1, j, s, t, dp);
         else return dp[i][j]= solve(i-1, j, s, t, dp);
     }
-    int numDistinct(string s, string t) {
+    long long numDistinct(string s, string t) {
         int n= s.size(), m= t.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return solve(n-1, m-1, s, t, dp);
+        vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+        // return solve(n-1, m-1, s, t, dp);
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int j=1;j<=m;j++) dp[0][j]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]= dp[i-1][j-1]+ dp[i-1][j];
+                else dp[i][j]= dp[i-1][j];
+            }
+        }
+        return (long long)dp[n][m];
     }
 };
