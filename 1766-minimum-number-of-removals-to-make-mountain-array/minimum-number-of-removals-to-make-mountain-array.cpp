37d7577310571fc86cbcdd52d1a4830a7d6fa891
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-06 15:12:54 +0530

    Time: 251 ms (72.93%) | Memory: 12.3 MB (20.82%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -3,14 +3,14 @@ public:
     int minimumMountainRemovals(vector<int>& arr) {
         int n= arr.size();
         vector<int> dp(n,1);
-        for(int ind=1;ind<n;ind++){
+        for(int ind=0;ind<n;ind++){
             for(int prev=0; prev<ind; prev++){
                 if(arr[ind]>arr[prev] && dp[ind]< dp[prev]+1) dp[ind] = dp[prev]+1;
             }
         }
         int maxlen=0;
         vector<int> cp(n,1);
-        for(int ind=n-2;ind>=0;ind--){
+        for(int ind=n-1;ind>=0;ind--){
             for(int prev=n-1; prev>=ind; prev--){
                 if(arr[ind]>arr[prev] && cp[ind]< cp[prev]+1) cp[ind] = cp[prev]+1;
             }
