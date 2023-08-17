1254f9b8a86bb096ed82f73caf7b7176071b120e
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-17 22:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (36.73%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    // bool valid(string s){
-    //     stack<char> st;
-    //     for(int i=0; i<s.size();i++){
-    //         if(s[i]=='('){
-    //             st.push('(');
-    //         }
-    //         else{
-    //             if(st.empty()){
-    //                 return false;
-    //             }
-    //             else{
-    //                 st.pop();
-    //             }
-    //         }
+    // void f(int open , int close, int n, vector<string>& res, string output, int ind){
+    //     if(ind==2*n){
+    //         res.push_back(output);
+    //         return ;
     //     }
-    //     return true;
+    //     if(open < n && ind<2*n-1){
+    //         output+= '(';
+    //         f(open+1, close, n, res, output, ind+1);
+    //         output.pop_back();
+    //     }
+    //     if(close<open && ind>0){
+    //         output+= ')';
+    //         f(open, close+1, n, res, output, ind+1);
+    //         output.pop_back();
+    //     }
     // }
-
-    void f(int open , int close, int n, vector<string>& res, string output, int ind){
-        if(ind==2*n){
+    void f(int open,int close,string output,vector<string> &res){
+        if(open == 0 && close == 0){
             res.push_back(output);
-            return ;
+            return;
         }
-        if(open < n && ind<2*n-1){
-            output+= '(';
-            f(open+1, close, n, res, output, ind+1);
-            output.pop_back();
-        }
-        if(close<open && ind>0){
-            output+= ')';
-            f(open, close+1, n, res, output, ind+1);
-            output.pop_back();
+        if(open == close) f(open-1,close,output+'(',res);
+        else if(open == 0) f(open,close-1,output+')',res);
+        else{
+            f(open-1,close,output+'(',res);
+            f(open,close-1,output+')',res);
         }
     }
+
     vector<string> generateParenthesis(int n) {
         string output="";
         vector<string> res;
-        f(0, 0, n, res, output, 0);
+        f(n, n, output, res);
         return res;
     }
 };
