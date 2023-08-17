7d8dabf983dbef276b281d39a56e918bc6942d62
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-17 23:48:46 +0530

    Time: 4 ms (60.31%) | Memory: 15.1 MB (33.43%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    // void f(int open , int close, int n, vector<string>& res, string output, int ind){
-    //     if(ind==2*n){
-    //         res.push_back(output);
-    //         return ;
-    //     }
-    //     if(open < n && ind<2*n-1){
-    //         output+= '(';
-    //         f(open+1, close, n, res, output, ind+1);
-    //         output.pop_back();
-    //     }
-    //     if(close<open && ind>0){
-    //         output+= ')';
-    //         f(open, close+1, n, res, output, ind+1);
-    //         output.pop_back();
-    //     }
-    // }
     void f(int open,int close,string output,vector<string> &res){
         if(open == 0 && close == 0){
             res.push_back(output);
