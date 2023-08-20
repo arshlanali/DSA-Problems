3241fb5381287f192ba7dbd6dfa6d24108d533eb
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-20 14:27:39 +0530

    Time: 104 ms (79.79%) | Memory: 42.7 MB (56.77%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,63 @@
+class disjointset{
+    vector<int> rank, parent, size;
+public:
+    disjointset(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1,0);
+        size.resize(n+1, 1);
+        for(int i=1; i<=n ; i++) parent[i]=i;
+    }
+    int findpar(int node){
+        if(node== parent[node]) return node;
+        return parent[node]= findpar(parent[node]);
+    }
+
+    void unionbyrank(int u, int v){
+        int pu= findpar(u);
+        int pv= findpar(v);
+        if(pu == pv) return; 
+        if(rank[pu]> rank[pv]){
+            parent[pv]=  pu;
+        }
+        else if(rank[pv]> rank[pu]){
+            parent[pu]= pv;
+        }
+        else{
+            parent[pv]= pu;
+            rank[pu]++; 
+        }
+    }
+
+    void unionbysize(int u, int v){
+        int pu= findpar(u);
+        int pv= findpar(v);
+        if(pu == pv) return; 
+        if(size[pu]> size[pv]){
+            parent[pv]=  pu;
+            size[pu]+= size[pv];
+        }
+        else if(rank[pv]>=rank[pu]){// we can also use the else statement here
+            parent[pu]= pv;
+            size[pv]+= size[pu];
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        disjointset ds(n);
+        int extra=0;
+        for(auto it:connections){
+            if(ds.findpar(it[0])== ds.findpar(it[1])) extra++;
+            else{
+                ds.unionbysize(it[0], it[1]);
+            }
+        }
+        int ncomp=0;
+        for(int i=0; i<n;i++){
+            if(ds.findpar(i)==i) ncomp++;
+        }
+        if(extra<ncomp-1) return -1;
+        return ncomp-1;
+    }
+};
