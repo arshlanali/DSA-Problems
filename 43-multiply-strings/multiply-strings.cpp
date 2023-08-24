2b43e2216542e1123e45c25baa5400eae0a1013a
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-24 18:24:03 +0530

    Time: 3 ms (92.41%) | Memory: 6.7 MB (68.10%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        if(num1=="0" or num2=="0") return "0";
-        int n= num1.size(), m= num2.size();
-        vector<int> vec(n+m, 0);
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0; j--){
-                vec[i+1+j]+= (num1[i]-'0')*(num2[j]-'0');
-                vec[i+j]+= vec[i+1+j]/10;
-                vec[i+1+j]= vec[i+1+j]%10;
-            }
-        }
-        // for(int i=0;i<n+m;i++) cout<<vec[i]<<" ";
-        string res="";
-        int i=0;
-        while(vec[i]==0) i++;
-        for(;i<n+m;i++) res+=  vec[i]+'0';
-        return res;
+    string multiply(string s, string t) {
+        int n= s.size(), m= t.size();
+        if(s=="0" || t=="0")  return "0";
+       int sneg=0, tneg=0;
+       if(s[0]=='-'){
+           sneg^=1;
+           s= s.substr(1);
+       } 
+       if(t[0]=='-') {
+           tneg^=1;
+           t= t.substr(1);
+       }
+       vector<int> vec(n+m);
+       for(int i=n-1; i>=0; i--){
+           for(int j=m-1; j>=0; j--){
+                vec[i+j+1]+= (s[i]-'0')*(t[j]-'0');
+                vec[i+j]+= vec[i+j+1]/10;
+                vec[i+j+1]= vec[i+j+1]%10;
+           }
+       }
+       int i=0;
+       while(i<n+m && vec[i]==0) i++;
+       string res="";
+       if(sneg^tneg) res+='-';
+       for(; i<n+m;i++) res+=vec[i]+'0';
+       return res;
     }
 };
