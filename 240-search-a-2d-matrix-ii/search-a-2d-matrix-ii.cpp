d9993da5547416bff44c2f1ae9ec5847031dd6d6
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-26 00:52:33 +0530

    Time: 94 ms (65.75%) | Memory: 14.8 MB (44.66%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // vector<int> col;
-        int n= matrix.size(), m= matrix[0].size();
-        // for(int i=0; i<n;i++){
-        //     col.push_back(matrix[i][0]);
-        // }
-        // if(n==1 && m==1) return matrix[0][0]==target;
-        // // for(auto it: col) cout<<it<<" ";
-        // int i= lower_bound(begin(col), end(col) , target)- begin(col);
-        // cout<<"i "<< i<<" ";
-        // if(i>=n) return false;
-        // if(col[i]==target) return true;
-        // int pos=pos= lower_bound(begin(matrix[i]), end(matrix[i]), target)- begin(matrix[i]);;
-        // while(i>=0 && matrix[i][pos]!=target){
-        //     i--;
-        //     pos= lower_bound(begin(matrix[i]), end(matrix[i]), target)- begin(matrix[i]);
-        // }
         
-        // cout<<"pos "<< pos<<" ";
+        int n= matrix.size(), m= matrix[0].size();
         // return pos<n && matrix[i][pos]==target;
 
         int row = 0, col= m-1;
