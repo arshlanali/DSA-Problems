4c6b5348d4bac0369988f557f1127584b0ba92c3
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-08-28 22:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Help Classmates/help-classmates.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        stack<int> st;
+        vector<int> res(n);
+        for(int i=n-1; i>=0; i--){
+            while(st.size() && st.top()>=arr[i]) st.pop();
+            if(st.empty()) res[i]=-1;
+            else res[i]= st.top();
+            st.push(arr[i]);
+        }
+        return res;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+
+// } Driver Code Ends
