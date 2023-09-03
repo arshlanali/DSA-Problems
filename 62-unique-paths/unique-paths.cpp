ffe8cba68f685f7581be45574b2dc1fd9436176a
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-09-03 14:02:02 +0530

    Time: 4 ms (14.96%) | Memory: 6.5 MB (24.88%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -9,7 +9,22 @@ public:
         return dp[i][j]= left+up; 
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return helper(m, n, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==1 && j==1){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up= dp[i-1][j];
+                int left= dp[i][j-1];
+                dp[i][j]= up+left;
+            }
+        }
+        return dp[m][n];
     }
 };
