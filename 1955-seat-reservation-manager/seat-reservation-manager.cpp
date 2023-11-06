01699ec3f15f250d3cf0209b6ef36bf9e6b4129f
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-11-06 11:41:59 +0530

    Time: 242 ms (99.28%) | Memory: 141.9 MB (98.85%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/seat-reservation-manager.cpp
@@ -1,14 +1,20 @@
 class SeatManager {
     priority_queue<int,vector<int>, greater<int>> minheap;
+    int seat;
 public:
     SeatManager(int n) {
-        for(int i=1;i<=n;i++) minheap.push(i);
+        // for(int i=1;i<=n;i++) minheap.push(i);
+        seat=1;
     }
     
     int reserve() {
-        int res= minheap.top();
-        minheap.pop();
-        return res;
+        if(minheap.size()){
+            int res= minheap.top();
+            minheap.pop();
+            return res;
+        }
+        seat++;
+        return seat-1;
     }
     
     void unreserve(int x) {
