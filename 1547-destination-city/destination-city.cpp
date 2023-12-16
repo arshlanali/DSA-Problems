2018e6584e3811e0ececcc096e62a7837eaef49d
arshlanali <67588384+arshlanali@users.noreply.github.com>
2023-12-16 08:46:15 +0530

    Time: 12 ms (35.24%) | Memory: 12 MB (10.12%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -2,30 +2,11 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
         unordered_map<string, int> hash;
-        int cnt=0;
+        for(auto it: paths){
+            hash[it[0]]++;
+        }
         for(auto it:paths){
-            if(!hash[it[0]]) hash[it[0]]=cnt++;
-            if(!hash[it[1]]) hash[it[1]]= cnt++;
-        }
-        // for(auto it:hash){
-        //     cout<<it.first<<" "<<it.second<<" ";
-        // }
-        int n= hash.size();
-        // cout<<n<<" ";
-        vector<int> outdegree(n+1);
-        for(auto it:paths){
-            outdegree[hash[it[0]]]++;
-        }
-        int res=-1;
-        for(int i=1;i<=n;i++){
-            // cout<<outdegree[i]<<" ";
-            if(outdegree[i]==0){
-                res=i;
-                break;
-            }
-        }
-        for(auto it:hash){
-            if(it.second==res) return it.first;
+            if(hash[it[1]]==0) return it[1];
         }
         return "";
     }
